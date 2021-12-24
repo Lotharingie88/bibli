7bbb993d37fce3b852a1a81c47270ca70618c9ef
Lotharingie88 <sylvain_thouvenot@laposte.net>
2021-12-24 16:58:53 +0100

    v1 de bibli
    Signed-off-by: Lotharingie88 <sylvain_thouvenot@laposte.net>

---
+++ bibli.pas
@@ -0,0 +1,33 @@
+unit bibli;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
+  FireDAC.Phys.MySQLDef, FireDAC.Stan.Intf, FireDAC.Stan.Option,
+  FireDAC.Stan.Error, FireDAC.UI.Intf, FireDAC.Phys.Intf, FireDAC.Stan.Def,
+  FireDAC.Stan.Pool, FireDAC.Stan.Async, FireDAC.Phys, FireDAC.Phys.MySQL,
+  FireDAC.FMXUI.Wait, Data.DB, FireDAC.Comp.Client, FireDAC.Stan.Param,
+  FireDAC.DatS, FireDAC.DApt.Intf, FireDAC.DApt, FireDAC.Comp.DataSet;
+
+type
+  TForm1 = class(TForm)
+    FDPhysMySQLDriverLink1: TFDPhysMySQLDriverLink;
+    FDConnection1: TFDConnection;
+    FDQuery1: TFDQuery;
+  private
+    { Déclarations privées }
+  public
+    { Déclarations publiques }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.fmx}
+{$R *.LgXhdpiPh.fmx ANDROID}
+
+end.
---
+++ biblio.dpr
@@ -0,0 +1,14 @@
+program biblio;
+
+uses
+  System.StartUpCopy,
+  FMX.Forms,
+  bibli in 'bibli.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
